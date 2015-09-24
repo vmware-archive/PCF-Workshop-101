ade00d99c2bb219c01e9f0aa152b70f24fa47428
phopper-pivotal <phopper@pivotal.io>
2015-09-24 14:49:12 -0600

    remove cf-spring-mvc, its no longer needed (replaced by cf-spring-mvc-boot)

--- cf-spring-mvc/src/main/resources/data/sampledata.sql
+++
@@ -1,7 +0,0 @@
-insert into attendee (id, firstname, lastname, address, city, state, zipcode, phonenumber, emailaddress) values (5001, 'John', 'Smith', '123 Main St', 'Akron', 'OH', '44321', '330-123-4567', 'jsmith@gopivotal.com');
-insert into attendee (id, firstname, lastname, address, city, state, zipcode, phonenumber, emailaddress) values (5002, 'Sally', 'Struthers', '321 Oak St', 'Akron', 'OH', '44321', '614-123-4567', 'sstruthers@gopivotal.com');
-insert into attendee (id, firstname, lastname, address, city, state, zipcode, phonenumber, emailaddress) values (5003, 'Jane', 'Doe', '4444 High St', 'Columbus', 'OH', '43333', '330-123-4567', 'jdoe@gopivotal.com');
-
-insert into session (id, attendee_id, name, date, completed) values (100, 5002, 'Session 1', '2013-12-01', false);
-insert into session (id, attendee_id, name, date, completed) values (101, 5002, 'Session 2', '2013-12-01', true);
-
